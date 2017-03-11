07e025f1886473a6ac63110418ff15a5caa11fec
ratiuseby <ratiu_sebastian1@yahoo.com>
2017-03-11 15:06:48 +0200

    Add files via upload

---
+++ Serializare V.2/Employee.java
@@ -0,0 +1,11 @@
+
+public class Employee implements java.io.Serializable {
+	public String name;
+	   public String address;
+	   public transient int SSN;
+	   public int number;
+	   
+	   public void mailCheck() {
+	      System.out.println("Trimite mail la " + name + " " + address);
+	   }
+}
